ca16b81a1180123443f48a4eb45fb48c35c8bd68
ChoiSou0 <95004780+ChoiSou0@users.noreply.github.com>
2022-08-08 19:49:47 +0900

    8/08
    상하이동 코드가 문제가 생김...코드 꼬여서 갈아엎어야될듯...

--- Assets/Script/Space_Mgr.cs
+++ Assets/Script/Space_Mgr.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Linq;
 
 public enum SpaceState
 {
@@ -59,25 +60,131 @@ public class Space_Mgr : MonoBehaviour
                                 if (SelectList[0].Num + 1 == Obj.GetComponent<Space>().Num ||
                                     SelectList[1].Num + 1 == Obj.GetComponent<Space>().Num)
                                 {
-                                    ChangeSpace(SelectList[0], );
-                                    ChangeSpace(SelectList[1], );
+                                    BlankSpace("1");
+                                    if (SelectList.Count != 0)
+                                    { 
+                                        FindSpace(SelectList[0], "1");
+                                        FindSpace(SelectList[1], "1");
+                                    }
                                 }
                                 else if (SelectList[0].Num - 1 == Obj.GetComponent<Space>().Num ||
-                                    SelectList[1].Num - 1 == Obj.GetComponent<Space>().Num)
+                                         SelectList[1].Num - 1 == Obj.GetComponent<Space>().Num)
                                 {
-
+                                    BlankSpace("-1");
+                                    if (SelectList.Count != 0)
+                                    {
+                                        FindSpace(SelectList[0], "-1");
+                                        FindSpace(SelectList[1], "-1");
+                                    }
                                 }
-                                //else if ()
-                                //{
+                                else
+                                {
+                                    switch (Obj.transform.parent.name)
+                                    {
+                                        case "WidthLine1":
+                                            break;
+                                        case "WidthLine2":
+                                            if (SelectList[0].Num - 5 == Obj.GetComponent<Space>().Num ||
+                                                SelectList[1].Num - 5 == Obj.GetComponent<Space>().Num)
+                                            {
 
-                                //}
-                                //else if ()
+                                            }
+                                            break;
+                                        case "WidthLine3":
+                                            if (SelectList[0].Num - 6 == Obj.GetComponent<Space>().Num ||
+                                                SelectList[1].Num - 6 == Obj.GetComponent<Space>().Num)
+                                            {
+
+                                            }
+                                            break;
+                                        case "WidthLine4":
+                                            if (SelectList[0].Num - 7 == Obj.GetComponent<Space>().Num ||
+                                                SelectList[1].Num - 7 == Obj.GetComponent<Space>().Num)
+                                            {
+                                                
+                                            }
+                                            break;
+                                        case "WidthLine5":
+                                            if (SelectList[0].Num - 8 == Obj.GetComponent<Space>().Num ||
+                                                SelectList[1].Num - 8 == Obj.GetComponent<Space>().Num)
+                                            {
+                                                BlankSpace("8");
+                                                if (SelectList.Count != 0)
+                                                {
+                                                    FindSpace(SelectList[0], "8");
+                                                    FindSpace(SelectList[1], "8");
+                                                }
+                                            }
+                                            break;
+                                        case "WidthLine6":
+                                            if (SelectList[0].Num - 8 == Obj.GetComponent<Space>().Num ||
+                                                SelectList[1].Num - 8 == Obj.GetComponent<Space>().Num)
+                                            {
+
+                                            }
+                                            break;
+                                        case "WidthLine7":
+                                            if (SelectList[0].Num - 7 == Obj.GetComponent<Space>().Num ||
+                                                SelectList[1].Num - 7 == Obj.GetComponent<Space>().Num)
+                                            {
+
+                                            }
+                                            break;
+                                        case "WidthLine8":
+                                            if (SelectList[0].Num - 6 == Obj.GetComponent<Space>().Num ||
+                                                SelectList[1].Num - 6 == Obj.GetComponent<Space>().Num)
+                                            {
+
+                                            }
+                                            break;
+                                        case "WidthLine9":
+                                            if (SelectList[0].Num - 5 == Obj.GetComponent<Space>().Num ||
+                                                SelectList[1].Num - 5 == Obj.GetComponent<Space>().Num)
+                                            {
+
+                                            }
+                                            break;
+                                    }
+                                }
+                                //else if (SelectList[0].Num - Obj.GetComponent<Space>().Num <= 8 &&
+                                //         SelectList[0].Num - Obj.GetComponent<Space>().Num >= 4)
+                                //{
+                                //    switch (transform.parent.name)
+                                //    {
+                                        
+                                //    }
+                                //}    
+                                //else if (Obj.GetComponent<Space>().Num - SelectList[1].Num >= -8)
                                 //{
 
                                 //}
 
                                 break;
                             case 3:
+                                if (SelectList[0].Num + 1 == Obj.GetComponent<Space>().Num ||
+                                    SelectList[1].Num + 1 == Obj.GetComponent<Space>().Num ||
+                                    SelectList[2].Num + 1 == Obj.GetComponent<Space>().Num)
+                                {
+                                    BlankSpace("1");
+                                    if (SelectList.Count != 0)
+                                    {
+                                        FindSpace(SelectList[0], "1");
+                                        FindSpace(SelectList[1], "1");
+                                        FindSpace(SelectList[2], "1");
+                                    }
+                                }
+                                else if (SelectList[0].Num - 1 == Obj.GetComponent<Space>().Num ||
+                                         SelectList[1].Num - 1 == Obj.GetComponent<Space>().Num ||
+                                         SelectList[2].Num - 1 == Obj.GetComponent<Space>().Num)
+                                {
+                                    BlankSpace("-1");
+                                    if (SelectList.Count != 0)
+                                    {
+                                        FindSpace(SelectList[0], "-1");
+                                        FindSpace(SelectList[1], "-1");
+                                        FindSpace(SelectList[2], "-1");
+                                    }
+                                }
                                 break;
                         }
                         SelectList.Clear();
@@ -98,23 +205,145 @@ public class Space_Mgr : MonoBehaviour
         
     }
 
+    // 작은 순으로 재배열
     private void Asend()
     {
-
+        SelectList.OrderBy(o=>o.Num).ToList();
     }
 
+    // 큰 순으로 재배열
     private void Desend()
     {
-
+        SelectList.OrderByDescending(o=>o.Num).ToList();
     }
 
+    // 이동칸이 빈칸인지 검사하는 함수
+    private void BlankSpace(string Type)
+    {
+        switch(Type)
+        {
+            case "1":
+                for (int i = 0; i < SelectList.Count - 1; i++)
+                {
+                    for (int j = 0; j < SpaceList.Count; j++)
+                    {
+                        if (SelectList[i].Num + 1 == SpaceList[j].Num &&
+                            SpaceList[j].spaceState != SpaceState.none)
+                        {
+                            SelectList.Clear();
+                        }
+                    }
+                }
+                break;
+            case "-1":
+                for (int i = 0; i < SelectList.Count - 1; i++)
+                {
+                    for (int j = 0; j < SpaceList.Count; j++)
+                    {
+                        if (SelectList[i].Num + 1 == SpaceList[j].Num &&
+                            SpaceList[j].spaceState != SpaceState.none)
+                        {
+                            SelectList.Clear();
+                        }
+                    }
+                }
+                break;
+            case "8":
+                for (int i = 0; i < SelectList.Count - 1; i++)
+                {
+                    for (int j = 0; j < SpaceList.Count; j++)
+                    {
+                        switch (SelectList[i].transform.parent.name)
+                        {   
+                            case "WidthLine1":
+                                break;
+                            case "WidthLine2":
+                                if (SelectList[i].Num - 5 == SpaceList[j].Num &&
+                                    SpaceList[j].spaceState != SpaceState.none)
+                                    SelectList.Clear();
+                                break;
+                            case "WidthLine3":
+                                if (SelectList[i].Num - 6 == SpaceList[j].Num &&
+                                    SpaceList[j].spaceState != SpaceState.none)
+                                    SelectList.Clear();
+                                break;
+                            case "WidthLine4":
+                                if (SelectList[i].Num - 7 == SpaceList[j].Num &&
+                                    SpaceList[j].spaceState != SpaceState.none)
+                                    SelectList.Clear();
+                                break;
+                            case "WidthLine5":
+                                if (SelectList[i].Num - 8 == SpaceList[j].Num &&
+                                    SpaceList[j].spaceState != SpaceState.none)
+                                    SelectList.Clear();
+                                break;
+                            case "WidthLine6":
+                                if (SelectList[i].Num - 8 == SpaceList[j].Num &&
+                                    SpaceList[j].spaceState != SpaceState.none)
+                                    SelectList.Clear();
+                                break;
+                            case "WidthLine7":
+                                if (SelectList[i].Num - 7 == SpaceList[j].Num &&
+                                    SpaceList[j].spaceState != SpaceState.none)
+                                    SelectList.Clear();
+                                break;
+                            case "WidthLine8":
+                                if (SelectList[i].Num - 6 == SpaceList[j].Num &&
+                                    SpaceList[j].spaceState != SpaceState.none)
+                                    SelectList.Clear();
+                                break;
+                            case "WidthLine9":
+                                if (SelectList[i].Num - 5 == SpaceList[j].Num &&
+                                    SpaceList[j].spaceState != SpaceState.none)
+                                    SelectList.Clear();
+                                break;
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    // 클릭한 이동위치를 찾는 함수
+    private void FindSpace(Space Founded, string Type)
+    {
+        for (int i = 0; i < SpaceList.Count; i++)
+        {
+            switch (Type)
+            {
+                case "1":
+                    if (i == Founded.Num + 1)
+                    {
+                        ChangeSpace(Founded, SpaceList[i]);
+                        break;
+                    }
+                    break;
+                case "-1":
+                    if (i == Founded.Num - 1)
+                    {
+                        ChangeSpace(Founded, SpaceList[i]);
+                        break;
+                    }
+                    break;
+                case "8":
+                    if (i == Founded.Num - 8)
+                    {
+                        ChangeSpace(Founded, SpaceList[i]);
+                        break;
+                    }
+                    break;
+            }
+        }
+    }
+
+    // 클릭한 위치로 이동해주는 함수
     private void ChangeSpace(Space a, Space b)
     {
         b.spaceState = a.spaceState;
         a.spaceState = SpaceState.none;
     }
 
-
+    // 맨처음 세팅해주는 함수
     private void StartSetting()
     {
         for (int i = 0; i < 5; i++)
